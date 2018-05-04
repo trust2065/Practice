1bf9868834ef5eb45ffefe6861464101a19b66e9
Choco <trust2065@example.com>
2018-05-04 16:10:16 +1000

    ramdonly change the knight's location

--- reactdnd/src/App.js
+++ reactdnd/src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
 // import './App.css';
 import Board from "./components/Board";
 
 class App extends Component {
   render() {
-    return <div style={{ width: "500px", height: "500px" }}>
-        <Board knightPosition={[0, 0]} />
-      </div>;
+    const {knightPosition} = this.props;
+    return (
+        <div style={{ width: "500px", height: "500px" }}>
+          <Board knightPosition={knightPosition} />
+        </div>
+    );
   }
 }
 
--- reactdnd/src/index.js
+++ reactdnd/src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { observe } from "./components/Game";
+
+observe(knightPosition =>
+  ReactDOM.render(
+    <App knightPosition={knightPosition} />,
+    document.getElementById("root")
+  )
+);
 registerServiceWorker();
