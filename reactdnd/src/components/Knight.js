a0333fca41bedc9a9d886e01332651ce86e7195e
Choco <trust2065@example.com>
2018-05-04 15:45:43 +1000

    center the knight component in square

--- reactdnd/src/components/Knight.js
+++ reactdnd/src/components/Knight.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Knight extends Component {
   render() {
-    return <span style={{"font-size":"40px"}}>♘</span>;
+    return <span style={{"margin": "0 auto", "font-size":"40px"}}>♘</span>;
   }
 }
 
--- reactdnd/src/components/Square.js
+++ reactdnd/src/components/Square.js
@@ -10,7 +10,7 @@ class Square extends Component {
     const fill = black ? "black" : "white";
     const stroke = black ? "white" : "black";
 
-    return <div style={{ backgroundColor: fill, color: stroke, width: "100%", height: "100%" }}>
+    return <div style={{ display:"flex", backgroundColor: fill, color: stroke, width: "100%", height: "100%" }}>
         {this.props.children}
       </div>;
   }
