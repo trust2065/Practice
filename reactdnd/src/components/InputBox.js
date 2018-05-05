b7a733c7983fc76a0a2bfc9e7807eb3fb1ba796e
Choco <trust2065@example.com>
2018-05-06 00:50:08 +1000

    define Target

--- reactdnd/src/components/InputBox.js
+++ reactdnd/src/components/InputBox.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
-import { DragSource } from "react-dnd";
+import { DragSource, DropTarget } from "react-dnd";
 import {ItemTypes} from "../constants/ReactDndItemTypes";
+import _ from 'lodash'
 
 const inputBoxSource = {
     beginDrag(props) {
@@ -8,21 +9,33 @@ const inputBoxSource = {
     },
 };
 
-function collect(connect, monitor) {
+function collectSource(connect, monitor) {
     return {
       connectDragSource: connect.dragSource(),
     };
 }
 
+const inputBoxTarget = {
+};
+
+function collectTarget(connect, monitor) {
+    return {
+      connectDropTarget: connect.dropTarget(),
+    };
+}
+
 class InputBox extends Component {
     render() {
-        const { connectDragSource } = this.props;
-        const {no} = this.props;
-        return connectDragSource(<div style={{ border: "1px solid black", width: "200px", marginTop: "20px"}}>
+        const { connectDragSource, connectDropTarget, no } = this.props;
+        return connectDropTarget(connectDragSource(<div style={{ border: "1px solid black", width: "200px", marginTop: "20px"}}>
             <p>{no}</p>
             <input type="text"/>
-        </div>)
+        </div>))
     }
 }
 
-export default DragSource(ItemTypes.INPUTBOX, inputBoxSource, collect)(InputBox);
+export default _.flow([
+    DragSource(ItemTypes.INPUTBOX, inputBoxSource, collectSource),
+    DropTarget(ItemTypes.INPUTBOX, inputBoxTarget, collectTarget)
+])(InputBox);
+// export default DropTarget(ItemTypes.INPUTBOX, inputBoxTarget, collectTarget)(DragSource(ItemTypes.INPUTBOX, inputBoxSource, collectSource)(InputBox));
