672cb10d118717e28b97d0d5085e221c5da9a562
Choco <trust2065@example.com>
2018-05-06 00:25:23 +1000

    encapsule source

--- reactdnd/src/App.js
+++ reactdnd/src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 // import logo from './logo.svg';
 // import './App.css';
 import InputBox from "./components/InputBox";
-
+import { DragDropContext } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
 class App extends Component {
   render() {
-    const {knightPosition} = this.props;
     return (
         <div style={{ margin: "100px 0 0 100px"}}>
           <InputBox no={1}/>
@@ -15,4 +15,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default DragDropContext(HTML5Backend)(App);
--- reactdnd/src/components/InputBox.js
+++ reactdnd/src/components/InputBox.js
@@ -1,14 +1,28 @@
 import React, {Component} from "react"
+import { DragSource } from "react-dnd";
+import {ItemTypes} from "../constants/ReactDndItemTypes";
 
+const inputBoxSource = {
+    beginDrag(props) {
+        return {};
+    },
+};
+
+function collect(connect, monitor) {
+    return {
+      connectDragSource: connect.dragSource(),
+    };
+}
 
 class InputBox extends Component {
     render() {
+        const { connectDragSource } = this.props;
         const {no} = this.props;
-        return <div style={{ border: "1px solid black", width: "200px", marginTop: "20px"}}>
+        return connectDragSource(<div style={{ border: "1px solid black", width: "200px", marginTop: "20px"}}>
             <p>{no}</p>
             <input type="text"/>
-        </div>
+        </div>)
     }
 }
 
-export default InputBox;
+export default DragSource(ItemTypes.INPUTBOX, inputBoxSource, collect)(InputBox);
