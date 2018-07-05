05fd9350f8ec56483545480e919354dfbcf8daf8
Choco <trust2065@gmail.com>
2018-07-05 16:23:13 +1000

    scales

--- d3/d3.js
+++ d3/d3.js
@@ -1,12 +1,26 @@
-const dataArray = [20, 40, 50];
+const dataArray = [20, 40, 50, 60];
+
+const width = 500;
+const height = 500;
+const domainMax = dataArray[dataArray.length - 1];
+
+const widthScale = d3
+  .scaleLinear()
+  .domain([0, domainMax])
+  .range([0, width]);
+
+const colorScale = d3
+  .scaleLinear()
+  .domain([0, domainMax])
+  .range(["red", "blue"]);
 
 // svg container
 const canvas = d3
   .select("body")
   .append("svg")
   .style("color", "red")
-  .attr("width", 500)
-  .attr("height", 500);
+  .attr("width", width)
+  .attr("height", height);
 
 // empty selection
 const bars = canvas
@@ -14,6 +28,7 @@ const bars = canvas
   .data(dataArray)
   .enter()
   .append("rect")
-  .attr("width", data => data * 10)
+  .attr("width", data => widthScale(data))
   .attr("height", 50)
+  .attr("fill", data => colorScale(data))
   .attr("y", (data, i) => i * 100);
