806718db9dcc73118345cfd5abd91c7d1cfca3e3
Choco <trust2065@gmail.com>
2018-07-06 08:52:05 +1000

    enter, update, exit

--- d3/d3.js
+++ d3/d3.js
@@ -1,46 +1,32 @@
-const dataArray = [20, 40, 50, 60];
+const data = [10];
 
-const width = 500;
-const height = 500;
-const domainMax = dataArray[dataArray.length - 1];
-
-const widthScale = d3
-  .scaleLinear()
-  .domain([0, domainMax])
-  .range([0, width]);
-
-const colorScale = d3
-  .scaleLinear()
-  .domain([0, domainMax])
-  .range(["red", "blue"]);
-
-const axis = d3
-  .axisBottom()
-  .ticks(5)
-  .scale(widthScale);
-
-// svg container
 const canvas = d3
   .select("body")
   .append("svg")
-  .style("color", "red")
-  .attr("width", width)
-  .attr("height", height)
-  .append("g")
-  .attr("transform", "translate(20, 0)");
+  .attr("width", 500)
+  .attr("height", 500);
 
-// empty selection
-const bars = canvas
-  .selectAll("anything")
-  .data(dataArray)
-  .enter()
-  .append("rect")
-  .attr("width", data => widthScale(data))
-  .attr("height", 50)
-  .attr("fill", data => colorScale(data))
-  .attr("y", (data, i) => i * 100);
+const circle1 = canvas
+  .append("circle")
+  .attr("cx", 50)
+  .attr("cy", 150)
+  .attr("r", 25);
 
-canvas
-  .append("g")
-  .attr("transform", "translate(0, 400)")
-  .call(axis);
+const circle2 = canvas
+  .append("circle")
+  .attr("cx", 50)
+  .attr("cy", 250)
+  .attr("r", 25);
+
+const circles = canvas
+  .selectAll("circle")
+  .data(data)
+  // update
+  .attr("fill", "green")
+  // exit
+  .exit()
+  .attr("fill", "yellow");
+
+// enter:  data.length > existDOM.length
+// update: data.length = existDOM.length
+// exit:   data.length < existDOM.length
