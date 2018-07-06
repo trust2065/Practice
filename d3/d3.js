d1c06882fa00fbaf210aee09286701955c4adbdc
Choco <trust2065@gmail.com>
2018-07-06 14:13:48 +1000

    loading external data

--- d3/d3.js
+++ d3/d3.js
@@ -1,16 +1,30 @@
-const data = [10, 20, 30, 40, 50];
-
-console.log(data.sort(d3.descending));
-console.log(d3.min(data));
-console.log(d3.max(data));
-console.log(d3.extent(data));
-console.log(d3.sum(data));
-console.log(d3.mean(data));
-console.log(d3.median(data));
-console.log(d3.shuffle(data));
+const data = [
+  { name: "Jason", age: 15 },
+  { name: "Jeny", age: 10 },
+  { name: "Kay", age: 20 }
+];
 
 const canvas = d3
   .select("body")
   .append("svg")
-  .attr("width", 500)
-  .attr("height", 500);
+  .attr("width", 200)
+  .attr("height", 200);
+
+canvas
+  .selectAll("anything")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("width", data => data.age * 10)
+  .attr("height", 40)
+  .attr("y", (d, i) => i * 50);
+// .attr("fill", "blue");
+
+canvas
+  .selectAll("text")
+  .data(data)
+  .enter()
+  .append("text")
+  .attr("fill", "white")
+  .attr("y", (d, i) => i * 50 + 20)
+  .text(data => data.name);
